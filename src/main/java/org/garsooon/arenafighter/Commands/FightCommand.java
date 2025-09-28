3bd4690f749698cc22e57b9a05944633cbc43f18
Garsooon <carson12345678910@hotmail.com>
2025-09-28 21:38:56 +0900

    Add enable/disable command

--- src/main/java/org/garsooon/arenafighter/Commands/FightCommand.java
+++ src/main/java/org/garsooon/arenafighter/Commands/FightCommand.java
@@ -91,6 +91,14 @@ public class FightCommand implements CommandExecutor {
     }
 
     private boolean handleChallenge(Player challenger, String[] args) {
+        if (plugin instanceof org.garsooon.arenafighter.Arena.ArenaFighter) {
+            org.garsooon.arenafighter.Arena.ArenaFighter afPlugin = (org.garsooon.arenafighter.Arena.ArenaFighter) plugin;
+            if (!afPlugin.isArenaEnabled()) {
+                challenger.sendMessage(ChatColor.RED + "Arena Fighter is currently disabled.");
+                return true;
+            }
+        }
+
         if (args.length < 2) {
             challenger.sendMessage(ChatColor.RED + "Usage: /fight challenge <player> [wagerAmount]");
             return true;
