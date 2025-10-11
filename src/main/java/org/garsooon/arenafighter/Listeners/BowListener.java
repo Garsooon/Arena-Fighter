aeaa3ecbb7cad311bc4fcbf67b00864191b87f17
Garsooon <carson12345678910@hotmail.com>
2025-10-11 20:50:56 +0900

    Fix commit error, Bow disable no longer global

--- src/main/java/org/garsooon/arenafighter/Listeners/BowListener.java
+++ src/main/java/org/garsooon/arenafighter/Listeners/BowListener.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.garsooon.arenafighter.Arena.ArenaFighter;
 
@@ -21,11 +22,10 @@ public class BowListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        if (plugin.isBowDisabled()
+        if (plugin.getFightManager().isInFight(player)
                 && player.getItemInHand() != null
                 && player.getItemInHand().getType() == Material.BOW
-                && event.getAction().toString().contains("RIGHT"))
-        {
+                && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
             event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "Bows are currently disabled in Arena-Fighter!");
         }
